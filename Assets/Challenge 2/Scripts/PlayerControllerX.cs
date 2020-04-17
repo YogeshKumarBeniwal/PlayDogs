23adba48dc716e26b0d862e80edae1ee0ab3b43e
Yogesh Kumar <yogeshkumarbeniwal1999@gmail.com>
2020-04-17 15:19:47 +0530

    Added project files.

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private bool canInstantiate = true;
+    private float previousTime = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && canInstantiate)
+        {
+            canInstantiate = false;
+            previousTime = Time.time;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+
+        if(canInstantiate == false && Time.time-previousTime>3)
+        {
+            canInstantiate = true;
+        }
+    }
+}
